21f69e805e778fe3083c3e2108c0106ea17b9a67
Mr. Moeller <alexmoeller88@gmail.com>
2020-07-27 18:49:12 +0000

    I wrote a solution for project euler problem 1.

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    //This is a comment
+
+
+    int count = 0;
+
+
+    for(int x = 1; x < 10; x++){
+
+      if(x % 3 == 0 || x % 5 ==0){
+        
+        count = count + x;
+      }
+    }
+
+    System.out.println("The sum of all integers below 1000 is " + count);
+
+
+
   }
 }
