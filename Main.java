0f23b1236d4163c2b4cbc919d8b3b4b3d456c293
Mr. Moeller <alexmoeller88@gmail.com>
2020-07-29 12:46:54 +0000

    I deleted everything I had written to show a change in my code.

--- Main.java
+++ Main.java
@@ -4,19 +4,6 @@ class Main {
     //This is a comment
 
 
-    int count = 0;
-
-
-    for(int x = 1; x < 10; x++){
-
-      if(x % 3 == 0 || x % 5 ==0){
-        
-        count = count + x;
-      }
-    }
-
-    System.out.println("The sum of all integers below 1000 is " + count);
-
 
 
   }
